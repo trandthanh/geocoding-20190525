c722fa2b1ac91e8bbb9ab055168d635b2454e5d9
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-05-25 21:45:24 +0200

    Back finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  resources :flats, except: [:index]
+  root to: 'flats#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
